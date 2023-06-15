595e0cd224db9792f772e6446b4e3a38ae8171d2
Certifi-Cat <136530777+Certifi-Cat@users.noreply.github.com>
2023-06-15 06:17:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'module';
+document.getElementsByTagName('head')[0].appendChild(script);
 
+var signedOn = false;
+
+function verifyCreate() {
+  if(signedOn == false) {
+    window.location.replace("https://certifi-cat.github.io/signup");
+  }
+  else window.location.replace("https://certifi-cat.github.io/create");
+}
+
+function verifyAccount() {
+  if(signedOn == false) {
+    window.location.replace("https://certifi-cat.github.io/signup");
+  }
+  else window.location.replace("https://certifi-cat.github.io/account");
+}
+
+function makeNFT() {
+  const form = new FormData();
+  form.append('allowPlatformToOperateToken', 'true');
+  form.append('chain', 'goerli');
+
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'X-API-Key': ${{ secrets.VWKey }}
+    }
+  };
+}
