c0e7ed628e18ca9d5deb2459c233122853f0654d
Certifi-Cat <136530777+Certifi-Cat@users.noreply.github.com>
2023-06-16 02:28:01 +0000

    Update script.js

--- create/step-2/script.js
+++ create/step-2/script.js
@@ -1,1 +1,12 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'module';
+document.getElementsByTagName('head')[0].appendChild(script);
 
+function saveData() {
+  var name = document.getElementsByName('nftInfo')[0].value;
+  var desc = document.getElementsByName('nftInfo')[1].value;
+  var address = document.getElementsByName('nftInfo')[2].value;
+  var blockchain = IsNullOrEmpty(document.getElementsByName('nftInfo')[3].value.trim()) ? 'goerli' : document.getElementById('inputName').toString();
+  document.cookie = "name=" + name "; " + "desc=" + desc "; " + "address=" + address "; " + "blockchain=" + blockchain "; " + "path=/"
+}
